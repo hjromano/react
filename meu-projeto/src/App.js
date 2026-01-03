eb24cf12d2990ce709e18b8fbb846ddd289f075d
Higor Romano <higorjromano@gmail.com>
2026-01-03 14:12:19 -0300

    react

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá React!</h1>
+      <p>Meu primeiro codigo em React</p>
+     
+    </div>
+  );
+}
+
+export default App;
