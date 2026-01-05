4bd9db6b98be3a485da31c54ee04c62eb75628f7
Higor Romano <higorjromano@gmail.com>
2026-01-04 21:08:55 -0300

    react curso

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Pessoa from './components/Pessoa';
 
 function App() {
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <p>Meu primeiro codigo em React</p>
+      <Pessoa nome= "Higor" idade= "29" profissao= "Programador" foto= "https://via.placeholder.com/150" />
      
     </div>
   );
