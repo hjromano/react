c29830f0d096ecfc5cee36de1946c2784a2012f9
Higor Romano <higorjromano@gmail.com>
2026-01-14 15:21:13 -0300

    adicionando banco de dados

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -6,8 +6,8 @@ function Select({ text, name, options, handleOnChange, value}) {
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}>
-                <option>Selecione um opção</option>
+            <select name={name} id={name} onChange={handleOnChange} value={value || ''}>
+                <option>Selecione uma opção</option>
                 {options.map((option) => (
                     <option value={option.id} key={option.id}>{option.name}</option>
 
