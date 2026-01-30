294413ba6a6fd51199c9f0ccaaa472bd32529ae6
Higor Romano <higorjromano@gmail.com>
2026-01-30 10:05:48 -0300

    Componentização de formulario  pt1
    criei pasta form para o input e o substitui no formulario

--- costs/src/components/project/ProjectForm.js
+++ costs/src/components/project/ProjectForm.js
@@ -1,12 +1,12 @@
+import Input from '../form/Input'
+
+import styles from './ProjectForm.module.css'
+
 function ProjectForm() {
     return (
-        <form>
-           <div>
-             <input type="text" placeholder="Insira o nome do projeto" />
-          </div>
-          <div>
-             <input type="number" placeholder="Insira o orçamento total" />
-          </div>
+        <form className={styles.form}>
+           <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
+           <Input type="number" text="Orçamento total" name="budget" placeholder="Insira o orçamento total" />
           <div>
              <select name="category_id">
                 <option dis>Selecione uma categoria</option>
